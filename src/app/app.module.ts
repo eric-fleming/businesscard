6bdc1881af08c8e0e192680b3536fd48d789446c
Eric <ericfleming8@gmail.com>
2019-11-25 19:48:05 -0600

    installing firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// My Components
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { BusinesscardComponent } from './businesscard/businesscard.component';
 import { NewBusinesscardComponent } from './newbusinesscard/newbusinesscard.component';
 import { BusinesscardListComponent } from './businesscard-list/businesscard-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+// Firebase
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+// My services
+
+
+// Web Cam Eventually
 
 @NgModule({
   declarations: [
@@ -22,7 +37,13 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
