9b17e28f55e4418de004f3833f737c961e537ee7
Eric <ericfleming8@gmail.com>
2019-11-26 01:17:47 -0600

    attempting to search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../models/Card.model';
+import { BusinesscardsService } from '../service/businesscards.service';
+import { map } from 'rxjs/operators';
+import { from } from 'rxjs/internal/observable/from';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  businessCardList: Card[];
+  searchResults: Card[];
+
+  constructor(private service: BusinesscardsService) { }
 
   ngOnInit() {
+    this.getBusinessCards();
+  }
+
+  getBusinessCards() {
+    this.service.getBusinessCards().snapshotChanges()
+      .pipe(
+        map(data => data.map(d => ({
+          key: d.payload.key, ...d.payload.val()
+        })))
+      ).subscribe(cards => {
+        this.businessCardList = cards;
+      });
+  }
+
+  search(
+    firstname: HTMLInputElement, lastname: HTMLInputElement,
+    company: HTMLInputElement, email: HTMLInputElement, phone: HTMLInputElement): void {
+
+    let inputArray = [firstname.value, lastname.value, company.value, email.value, phone.value];
+
+    // Testing the fields
+    console.table(inputArray);
+    inputArray = inputArray.map( e => e.toLowerCase());
+    console.table(inputArray);
+
+    this.getBusinessCards();
+    const allCardsStream = from(this.businessCardList);
+
+    // I need to grab the results and filter the output.
+  }
+
+  clear(
+    firstname: HTMLInputElement, lastname: HTMLInputElement,
+    company: HTMLInputElement, email: HTMLInputElement, phone: HTMLInputElement): void {
+    // clear text fields
+    firstname.value = '';
+    lastname.value = '';
+    company.value = '';
+    email.value = '';
+    phone.value = '';
   }
 
 }
