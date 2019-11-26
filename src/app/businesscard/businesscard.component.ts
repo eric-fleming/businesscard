8edbfe801af6c822d7bceb1ca50ab2f6d80a4a2d
Eric <ericfleming8@gmail.com>
2019-11-25 20:49:49 -0600

    attempting to get b-card to render

--- src/app/businesscard/businesscard.component.ts
+++ src/app/businesscard/businesscard.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Card } from '../models/Card.model';
+import { BusinesscardsService } from '../service/businesscards.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-businesscard',
   templateUrl: './businesscard.component.html',
   styleUrls: ['./businesscard.component.css']
 })
-export class BusinesscardComponent implements OnInit {
+export class BusinesscardComponent implements OnInit, OnDestroy {
+
 
   @Input() cardInfo: Card;
   @Input() i: any;
 
-  constructor() { }
+  constructor(private businesscardsService: BusinesscardsService) { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getImageURL(): string {
+    return this.cardInfo.imageURL;
   }
 
 }
