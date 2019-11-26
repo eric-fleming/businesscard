c4f5221bbf74817627efd973289f594d7249f36a
Eric <ericfleming8@gmail.com>
2019-11-25 21:01:15 -0600

    setting up login and auth services

--- src/app/guards/auth-guard.guard.ts
+++ src/app/guards/auth-guard.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { take, map, tap } from 'rxjs/operators';
+import { AuthService } from '../service/auth.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>  {
+    return this.authService.user.pipe(
+      take(1),
+      map((user) => !!user),
+      tap((loggedIn) => {
+        if (!loggedIn) {
+          console.log('access denied');
+          this.router.navigate(['/login']);
+        }
+      }),
+    );
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+interface User {
+  uid: string;
+  email: string;
+  photoURL?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AuthService {
 
-  constructor() { }
+  user: Observable<User>;
+  message: string;
+
+  constructor(private firebaseAuth: AngularFireAuth, private router: Router) {
+    this.user = firebaseAuth.authState;
+  }
+
+  login(username: string, password: string) {
+    this.firebaseAuth.auth
+      .signInWithEmailAndPassword(username, password)
+      .then(value => {
+        localStorage.setItem('username', 'user');
+        this.message = '';
+        console.log('Nice, it worked!', value);
+        this.router.navigate(['/profile']);
+      })
+      .catch(err => {
+        this.message = 'Incorrect username or password';
+        console.log('Something went wrong:', err.message);
+      });
+  }
+
+  logout() {
+    this.firebaseAuth.auth.signOut()
+      .then(() => {
+        localStorage.removeItem('username');
+        this.router.navigate(['/login']);
+        console.log('Signed out successed');
+      });
+  }
+
+  getUser(): any {
+    return localStorage.getItem('username');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getUser();
+  }
 }
