da82a103ad1907960c742cb2d4ed97c0eea40224
Eric <ericfleming8@gmail.com>
2019-11-25 21:23:14 -0600

    modifying  from last homework

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    this.authService.login(this.loginForm.value.username, this.loginForm.value.password);
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
