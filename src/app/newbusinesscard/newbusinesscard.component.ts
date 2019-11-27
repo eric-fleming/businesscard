79e8cf678035fe4d2495f553a0c18d2d7d226089
Eric <ericfleming8@gmail.com>
2019-11-26 18:33:05 -0600

    takes snapshot

--- src/app/newbusinesscard/newbusinesscard.component.ts
+++ src/app/newbusinesscard/newbusinesscard.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Card } from '../models/Card.model';
 import { BusinesscardsService } from '../service/businesscards.service';
 
+// For the Webcamera
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 
 
 @Component({
@@ -11,9 +15,19 @@ import { BusinesscardsService } from '../service/businesscards.service';
 })
 export class NewBusinesscardComponent implements OnInit {
 
+  // toggle webcam on/off
+  public showWebcam = true;
+  public errors: WebcamInitError[] = [];
+
+  // latest snapshot
+  public webcamImage: WebcamImage = null;
+  // webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
+
   constructor(private service: BusinesscardsService) { }
 
   ngOnInit() {
+    // Use if you had multiple web cameras
   }
 
   submit(
@@ -29,5 +43,33 @@ export class NewBusinesscardComponent implements OnInit {
     email.value = '';
     phone.value = '';
   }
+
+  /****** WEBCAMERA METHODS *******/
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+
+  public toggleWebcam(): void {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+
+  // public showNextWebcam(directionOrDeviceId: boolean | string): void
+
+  public handleImage(webcamImage: WebcamImage): void {
+    // tslint:disable-next-line:no-console
+    console.info('received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+  }
+
+  // public cameraWasSwitched(deviceId: string): void
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+  // public get nextWebcamObservable(): Observable<boolean | string>
 
 }
